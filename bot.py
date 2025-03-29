04ca05f856f279b72e54275539eb771ab7b7494b
Allen Thomas <at2224@njit.edu>
2025-03-29 18:47:41 -0400

    successfully sends memes on command

--- bot.py
+++ bot.py
@@ -1,5 +1,20 @@
 import discord
+import requests 
+import json 
+"""
+Go to this link to create discord bot, enable permissions, get token, and make invite link:
+https://discord.com/developers/applications
 
+"""
+
+# getting memees from public api link in json format and returns reddit meme link
+def get_meme():
+    response = requests.get('https://meme-api.com/gimme')
+    json_data = json.loads(response.text)
+    return json_data['url']
+
+# class extends discord.py class which we installed
+# responds to discord messages starting with $
 class MyClient(discord.Client):
     async def on_ready(self):
         print('Logged on as {0}!'.format(self.user))
@@ -8,9 +23,13 @@ class MyClient(discord.Client):
             return 
         if message.content.startswith('$hello'):
             await message.channel.send('Hello World!')
+        if message.content.startswith('$meme'):
+            await message.channel.send(get_meme())
+    
 
+# set settings basically. set message_content to true to post messages
 intents = discord.Intents.default()
 intents.message_content = True
-
+# assigns settings to client and uses discord API token to run client
 client = MyClient(intents=intents)
 client.run('MTM1NTY2NTMwMDU2MDgwNjExMA.GkDwB-.YXY-x5ES8iApH2-b2LPh1Yl2Xn8seNKWlQ_mzM')
