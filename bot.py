bc7d3d2c3c29c831f7193d31932560c91137cfe8
Allen Thomas <at2224@njit.edu>
2025-03-29 18:30:10 -0400

    bot sends message in response to $hello

---
+++ bot.py
@@ -0,0 +1,16 @@
+import discord
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged on as {0}!'.format(self.user))
+    async def on_message(self, message):
+        if message.author == self.user:
+            return 
+        if message.content.startswith('$hello'):
+            await message.channel.send('Hello World!')
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = MyClient(intents=intents)
+client.run('MTM1NTY2NTMwMDU2MDgwNjExMA.GkDwB-.YXY-x5ES8iApH2-b2LPh1Yl2Xn8seNKWlQ_mzM')
